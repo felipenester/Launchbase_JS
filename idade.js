f6480af8be34da0fea412bd87511038e6b10cd64
felipenester <57141137+felipenester@users.noreply.github.com>
2020-06-19 10:26:27 -0300

    Exemplo de condicionais + modelo desafio
    utilização do modelo da aula, mas aplicando o modelo de condicionais proposto no desafio 1-1

---
+++ idade.js
@@ -0,0 +1,21 @@
+// DESAFIO IDADE
+const idade = 17
+
+// verificar se a pessoa é maior ou igual a 18 anos
+// se sim, deixar entrar, se não, bloquear entrada
+// se a pessoa tiver 17 anos 
+// avisar para voltar quando fizer 18 anos
+
+if(!(idade >= 18)){
+     if (idade == 17){
+         console.log('Volte quando fizer 18 anos')
+    } else {
+    console.log('Não deixar entrar')
+    }
+} else {
+    console.log('Deixar entrar')
+}
+
+
+
+
